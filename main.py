16129f3213b4747a6bd3aa041c7bd7106249fb35
lephuock <phuockhanhnk94@gmail.com>
2022-04-25 22:58:20 +0200

    from pretrained function

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ if __name__ == '__main__':
     # Hyper parameters TODO: fix with argparse
     params = {"model_image_batch_size": 64, "model_image_epoch": 5,
               "model_voice_batch_size": 32, "model_voice_epoch": 10,
-              "model_mix_batch_size": 32, "model_mix_epoch": 10,
+              "model_mix_batch_size": 32, "model_mix_epoch": 4,
               }
 
     # Train image model
     x_train_image, x_test_image, y_train_image, y_test_image = preprocess.build_dataset_image()
-    image_model = model_image.ImageModel()
-    model_utils.optimize_model(image_model)
-    model_utils.train(model=image_model, x_train=x_train_image, y_train=y_train_image,
-                      batch_size=params['model_image_batch_size'],
-                      epochs=params['model_image_epoch'])
-    print("validation of image model only")
-    image_model.evaluate(x_test_image, y_test_image)
+    # image_model = model_image.ImageModel()
+    # model_utils.optimize_model(image_model)
+    # model_utils.train(model=image_model, x_train=x_train_image, y_train=y_train_image,
+    #                   batch_size=params['model_image_batch_size'],
+    #                   epochs=params['model_image_epoch'])
+    # print("validation of image model only")
+    # image_model.evaluate(x_test_image, y_test_image)
 
     # Train voice model
     x_train_voice, x_test_voice, y_train_voice, y_test_voice = preprocess.build_dataset_voice("./data/prepro_dataset")
-    voice_model = model_voice.VoiceModel()
-    model_utils.optimize_model(voice_model)
-    model_utils.train(model=voice_model, x_train=x_train_voice, y_train=y_train_voice,
-                      batch_size=params['model_voice_batch_size'],
-                      epochs=params['model_voice_epoch'])
-    print("validation of voice model only")
-    voice_model.evaluate(x_test_voice, y_test_voice)
+    # voice_model = model_voice.VoiceModel()
+    # model_utils.optimize_model(voice_model)
+    # model_utils.train(model=voice_model, x_train=x_train_voice, y_train=y_train_voice,
+    #                   batch_size=params['model_voice_batch_size'],
+    #                   epochs=params['model_voice_epoch'])
+    # print("validation of voice model only")
+    # voice_model.evaluate(x_test_voice, y_test_voice)
 
     # Mixed model
     x_train_image, x_test_image, = preprocess.build_dataset_mixed(num_classes=10,
@@ -42,7 +42,9 @@ if __name__ == '__main__':
                                                                   y_train_image=y_train_image,
                                                                   y_test_image=y_test_image)
 
-    mix_model = model_mix.MixedModel(image_model, voice_model)
+    # mix_model = model_mix.MixedModel(image_model, voice_model)
+    mix_model = model_mix.MixedModel.from_pretrain(image_model_path="./data/pretrain_model/image_model",
+                                                   voice_model_path="./data/pretrain_model/voice_model")
     model_utils.optimize_model(mix_model)
     model_utils.train(model=mix_model, x_train=[x_train_voice, x_train_image],
                       y_train=[y_train_voice, y_train_voice],
@@ -57,9 +59,9 @@ if __name__ == '__main__':
                                                                       [x_test_voice, np.zeros(x_test_image.shape)],
                                                                       [y_test_voice, y_test_voice])
 
-    print("save model")
-    image_model.save("./data/pretrain_model/image_model")
-    voice_model.save("./data/pretrain_model/voice_model")
+    # print("save model")
+    # image_model.save("./data/pretrain_model/image_model")
+    # voice_model.save("./data/pretrain_model/voice_model")
     # mix_model.save("./data/pretrain_model/mix_model")
 
     with mlflow.start_run() as run:
---
+++ model/model_image_distill.py
@@ -0,0 +1,9 @@
+# import keras
+# from model.model_mix import MixedModel
+#
+#
+# class ImageModelDistill(keras.Model):
+#     def __init__(self, mix_model: MixedModel):
+#         super(ImageModelDistill, self).__init__()
+#         self.base = mix_model.base_image
+#
